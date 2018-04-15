ad2538f844ef1b5ff0200de98a2fc3dda6f7ca83
Demon Singur <demonsingur@gmail.com>
2018-04-15 10:55:05 +0000

    msm8953-common: Convert lights HAL into a native binderized HAL
    Change-Id: I4fa56abd38c04d8fa267dbab67e54fe4a617bfe1
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -167,9 +167,6 @@ JAVA_SOURCE_OVERLAYS := org.lineageos.hardware|$(VENDOR_PATH)/lineagehw|**/*.jav
 # Keymaster
 TARGET_PROVIDES_KEYMASTER := true
 
-# Lights
-TARGET_PROVIDES_LIBLIGHT := true
-
 # Media
 TARGET_USES_MEDIA_EXTENSIONS := true
 
