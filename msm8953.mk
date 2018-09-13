57dc41171dd899a9a2c035d417684895b43354fc
Ramjee Singh <ramjee@codeaurora.org>
2018-09-13 15:31:22 +0530

    msm8953-common: Version upgrade for soundtrigger HAL to 2.1
    Change-Id: If1df46249fc5d69ac463f9248f44d848acd4155d
    
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -79,7 +79,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@4.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@4.0-impl \
-    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
     audio.primary.msm8953 \
     audio.r_submix.default \
