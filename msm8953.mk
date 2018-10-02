fe40295cbd715ec8bb52e84f6501d1957626460a
Dede Dindin Qudsy <xtrymind@gmail.com>
2018-10-02 20:52:00 +0700

    msm8953-common: only enforce framework-res
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -22,12 +22,8 @@ $(call inherit-product, vendor/xiaomi/msm8953-common/msm8953-common-vendor.mk)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
-#PRODUCT_ENFORCE_RRO_TARGETS := \
-    Bluetooth \
-    Settings \
-    SettingsProvider \
-    SystemUI \
-    framework-res \
+PRODUCT_ENFORCE_RRO_TARGETS := \
+    framework-res
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
