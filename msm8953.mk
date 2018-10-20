428dcad37b3cf660c8f06cbb87433ea547524397
Dede Dindin Qudsy <xtrymind@gmail.com>
2018-10-20 11:35:27 +0700

    msm8953-common: build vendor.display.config
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -144,7 +144,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     vendor.display.color@1.0-service \
-    vendor.display.color@1.0-impl
+    vendor.display.color@1.0-impl \
+    vendor.display.config@1.1 \
+    vendor.display.config@1.1_vendor
 
 # Doze mode
 PRODUCT_PACKAGES += \
