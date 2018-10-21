5133f2a07ea73b8e878ce89008b75a4b5eded56b
Dede Dindin Qudsy <xtrymind@gmail.com>
2018-10-21 11:15:22 +0700

    msm8953-common: set PRODUCT_VENDOR_MOVE_ENABLED
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -220,6 +220,7 @@ BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
 WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+PRODUCT_VENDOR_MOVE_ENABLED := true
 
 # Inherit from the proprietary version
 -include vendor/xiaomi/msm8953-common/BoardConfigVendor.mk
