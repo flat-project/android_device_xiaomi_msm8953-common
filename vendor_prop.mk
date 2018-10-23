a4d64467c2a745b4f1e94a5564fb380215b9d65b
Dede Dindin Qudsy <xtrymind@gmail.com>
2018-10-23 08:29:38 +0700

    msm8953-common: Set lmkd configuration properties
    taken from pixel 3
    
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -137,6 +137,18 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.hwui.text_large_cache_width=2048 \
     ro.hwui.text_large_cache_height=1024
 
+#LKMD
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.lmk.low=1001 \
+    ro.lmk.medium=800 \
+    ro.lmk.critical=0 \
+    ro.lmk.critical_upgrade=false \
+    ro.lmk.upgrade_pressure=100 \
+    ro.lmk.downgrade_pressure=100 \
+    ro.lmk.kill_heaviest_task=true \
+    ro.lmk.kill_timeout_ms=100 \
+    ro.lmk.use_minfree_levels=true
+
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
     av.debug.disable.pers.cache=1 \
